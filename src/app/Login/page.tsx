4b8556e16cca137a56a14752a66453afd56706fa
Lukas Pinheiro <lukas.pinheiro123@gmail.com>
2024-07-12 11:46:13 -0400

    alteração na pagina de login

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -14,7 +14,7 @@ export default function Login() {
     }
 
     return (
-        <div className="w-full h-screen bg-teal-900 flex justify-center items-center">
+        <div className="w-full h-screen flex justify-center items-center bg-gradient-to-r from-teal-900 to-teal-300">
             <div className="flex flex-col gap-3 bg-white p-16 rounded-lg">
                 <Input
                     label="Login"
@@ -27,7 +27,7 @@ export default function Login() {
                     setData={setPassword}
                 />
 
-                <button onClick={onSubmit}>Entrar</button>
+                <button className="border-2 py-2 px-5 rounded-full bg-teal-500 shadow hover:shadow-md hover:bg-gradient-to-r from-teal-900 to-teal-500" onClick={onSubmit}>Entrar</button>
             </div>
         </div>
     )
--- src/app/components/Input/index.tsx
+++ src/app/components/Input/index.tsx
@@ -8,7 +8,7 @@ export default function Input({ label, data, setData }: InputProps) {
     return (
         <>
             <label htmlFor="password">{label}</label>
-            <input onChange={(text)=> setData(text.target.value)} value={data} className="border py-2 px-5 rounded-full bg-gray" id="password" />
+            <input onChange={(text)=> setData(text.target.value)} value={data} className="shadow hover:shadow-md border-2 py-2 px-5 rounded-full bg-gray" id="password" />
         </>
     )
 }
