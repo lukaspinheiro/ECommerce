7de1fa93c16f319bba3ba0142d21a9eb2668068b
Lukas Pinheiro <lukas.pinheiro123@gmail.com>
2024-07-16 03:05:46 -0400

    conexão com api

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,16 +1,30 @@
 'use client'
 
 import { FipeApi } from "@/api/FipeApi"
+import { log } from "console"
 import { useEffect, useState } from "react"
 
 
 export default function Home() {
     const [brand, setBrand] = useState<any[]>([])
     const [models, setModels] = useState<any[]>([])
+    const [year, setYear] = useState<any[]>([])
+    const [value, setValue] = useState<any[]>([])
+
+    const [codBrand, setCodBrand] = useState<string>()
+    const [codModel, setCodModel] = useState<string>()
+    const [vehicle, setVehicle] = useState<string>("")
+    const [fuel, setFuel] = useState<string>("")
+
+
+    const onChangeRadio = (id:string)=>{
+        setVehicle(id)
+        fetchBrand()
+    }
 
     const fetchBrand = async () => {
         try {
-            const res = await FipeApi.get('carros/marcas')
+            const res = await FipeApi.get(`${vehicle}/marcas`)
             setBrand(res.data)
         }
         catch (err) {
@@ -18,42 +32,68 @@ export default function Home() {
         }
     }
 
+
     const onChangeBrand = async (codigo: any) => {
         try {
-            const res = await FipeApi.get(`carros/marcas/${codigo}/modelos`)
-            console.log(res);
+            const res = await FipeApi.get(`${vehicle}/marcas/${codigo}/modelos`)
             setModels(res.data.modelos)
+            setCodBrand(codigo)
         }
         catch (err) {
             console.log(err);
         }
     }
 
+    const onChangeModel = async (codigo: any) => {
+        try{
+            const res = await FipeApi.get(`${vehicle}/marcas/${codBrand}/modelos/${codigo}/anos`)
+            setYear(res.data)
+            setCodModel(codigo)
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+
+    const onChangeYear = async (codigo: any)  =>{
+        try{
+            const res = await FipeApi.get(`${vehicle}/marcas/${codBrand}/modelos/${codModel}/anos/${codigo}`)
+            console.log(res.data)
+            setValue(res.data)
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+
     useEffect(() => {
-        fetchBrand()
-    }, [])
+        if (vehicle!==""){
+            fetchBrand()
+        }
+    }, [vehicle])
 
     return (
         <div className="w-full h-screen flex justify-center items-center bg-gradient-to-r from-teal-900 to-teal-300">
             <div className="flex flex-col bg-white p-8 rounded-lg ">
                 <div className="flex flex-row gap-6 bg-white p-8 rounded-lg">
                     <ul className="flex flex-col justify-between gap-2 bg-whiterounded-lg">
+                        
                         <li className="shadow-md border-2 border-slate-400 rounded-lg grid justify-items-center pt-2">
-                            <input type="radio" name="" id="radio-car" />
+                            <input type="radio" name="radioVehicle" id="carros" onChange={(e)=>onChangeRadio("carros")}/>
                             <label htmlFor="radio-car" className="shadow-md hover:shadow-lg">
                                 <img src="../images/iconCar.png" className="w-8 md:w-16 lg:w-32" alt="" />
                             </label>
                         </li>
 
                         <li className="shadow-md border-2 border-slate-400 rounded-lg grid justify-items-center pt-2">
-                            <input type="radio" name="" id="radio-motorcycle" />
+                            <input type="radio" name="radioVehicle" id="motos" onChange={(e)=>onChangeRadio("motos")}/>
                             <label htmlFor="radio-motorcycle" className="shadow-md hover:shadow-lg">
                                 <img src="../images/iconMotorcycle.png" className="w-8 md:w-16 lg:w-32" alt="" />
                             </label>
                         </li>
 
                         <li className="shadow-md border-2 border-slate-400 rounded-lg grid justify-items-center pt-2">
-                            <input type="radio" name="" id="radio-truck" />
+                            <input type="radio" name="radioVehicle" id="caminhoes" onChange={(e)=>onChangeRadio("caminhoes")}/>
                             <label htmlFor="radio-truck" className="shadow-md hover:shadow-lg">
                                 <img src="../images/iconTruck.png" className="w-8 md:w-16 lg:w-32" alt="" />
                             </label>
@@ -76,7 +116,7 @@ export default function Home() {
                     </select>
 
                     <label htmlFor="selectModelo">Modelo</label>
-                    <select id="selectModelo" onChange={(e) => console.log(e.target.value)} className="shadow-md hover:shadow-lg border border-slate-400 py-2 rounded-lg">
+                    <select id="selectModelo" onChange={(e) => onChangeModel(e.target.value)} className="shadow-md hover:shadow-lg border border-slate-400 py-2 rounded-lg">
                         <option>Selecione uma Opção</option>
                         {
                             models.map((b, i) => (
@@ -86,7 +126,14 @@ export default function Home() {
                     </select>
 
                     <label htmlFor="selectAno">Ano</label>
-                    <select name="" id="selectAno" className="shadow-md hover:shadow-lg border border-slate-400 py-2 rounded-lg"></select>
+                    <select name="" id="selectAno" onChange={(e) => onChangeYear(e.target.value)} className="shadow-md hover:shadow-lg border border-slate-400 py-2 rounded-lg">
+                        <option>Selecione uma Opção</option>
+                        {
+                            year.map((b, i) => (
+                                <option key={i} value={b.codigo}>{b.nome}</option>
+                            ))
+                        }
+                    </select>
                 </div>
 
                 <div className="flex flex-row gap-8 bg-white pb-6 rounded-lg">
